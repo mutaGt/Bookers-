fefed69578ab7ee82d4f41945f90cc04b7fae529
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-06-11 07:30:40 +0000

    [Add]バリデーションの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/'=>'homes#top'
   resources :books
+  
+  patch 'books/:id'=>'books#update',as: 'update_book'
+  delete 'books/:id'=> 'books#destroy',as: 'destroy_book'
+  
+  
 end
